74c69f27388fac9e7e493d9dd779af4b7a93cb44
debora958 <109834177+debora958@users.noreply.github.com>
2022-08-26 16:22:29 -0300

    Add files via upload

---
+++ string.js
@@ -0,0 +1,6 @@
+let endereço = "avenida conde da boa vista".toUpperCase()
+let cidade = " RECIFE".toLowerCase() 
+let quantidade = cidade.length 
+
+console.log(`${cidade} tem ${quantidade} letras e é a cidade que está localizada a: ${endereço}`)
+
